b3dbcd2ba1a1d587c795058becf2f307d9003d34
Naganathan <93819633+Nagavshiva@users.noreply.github.com>
2022-08-24 19:14:50 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { User } = require("../models/user");
 const Token = require("../models/token");
 const crypto = require("crypto");
 const sendEmail = require("../utils/sendEmail");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Joi = require("joi");
 
 router.post("/", async (req, res) => {
